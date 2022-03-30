b4bc54584296120044668555a0b510edf581adb7
Pablo <alu0101225364@ull.edu.es>
2022-03-30 14:00:52 +0100

    Test that cards are 1-10 or JQK green

--- src/cards.ts
+++ src/cards.ts
@@ -8,8 +8,6 @@ const valuesForLetters: {[key:string]:string} = {
   "K":"13"
 }
 class Cards{
-
-
   compare(card1:string, card2:string): string{
     if(card1.length > 1 || card2.length > 1){
       throw new TypeError("length of cards greater than 1");
@@ -19,10 +17,13 @@ class Cards{
     if(valuesForLetters.hasOwnProperty(card1)){
       card1 = valuesForLetters[card1];
     }
-    if(valuesForLetters.hasOwnProperty(card1)){
-      card1 = valuesForLetters[card1];
+    if(valuesForLetters.hasOwnProperty(card2)){
+      card2 = valuesForLetters[card2];
     }
-
+    console.log(card1 + card2)
+    if(isNaN(Number(card1)) || isNaN(Number(card2))){
+      throw new TypeError("One of the cards is not correct");
+    }
     if(Number(card1) > Number(card2)){
       return originalCard1;
     } else return originalCard2;  
