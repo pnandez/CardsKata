a89975fd270ac237b9eb52a70a94e8011cdad249
Pablo <alu0101225364@ull.edu.es>
2022-03-30 13:25:58 +0100

    Refactor made for reducing complexity

--- src/cards.ts
+++ src/cards.ts
@@ -1,30 +1,24 @@
 import { isNumberLiteral } from "@babel/types";
 import { cachedDataVersionTag } from "v8";
 
+
+const valuesForLetters: {[key:string]:string} = {
+  "J":"11",
+  "Q":"12",
+  "K":"13"
+}
 class Cards{
+
 
   compare(card1:string, card2:string): string{
     let originalCard1:string = card1;
     let originalCard2:string = card2;
-    if(card1.match("J")){
-      card1 = "11";
+    if(valuesForLetters.hasOwnProperty(card1)){
+      card1 = valuesForLetters[card1];
     }
-    if(card1.match("Q")){
-      card1 = "12";
-    }
-    if(card1.match("K")){
-      card1 = "13";
-    }
-    if(card2.match("J")){
-      card2 = "11";
-    }
-    if(card2.match("Q")){
-      card2 = "12";
-    }
-    if(card2.match("K")){
-      card2 = "13";
-    }
-    if(Number(card1) > Number(card2)){
+    if(valuesForLetters.hasOwnProperty(card1)){
+      card1 = valuesForLetters[card1];
+    }    if(Number(card1) > Number(card2)){
       return originalCard1;
     } else return originalCard2;  
   
