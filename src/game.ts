db92bfb61dab2f1669feaeb949f098b8f51734db
Pablo <alu0101225364@ull.edu.es>
2022-03-30 18:03:35 +0100

    Test game for winner in green

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,11 @@
-import Cards from "./cards";
+import cards from "./cards";
 
 class Game{
 
   readonly cardsP1:string[] = [];
+  private _pointsP1:number = 0;
   readonly cardsP2:string[] = [];
+  private _pointsP2:number = 0;
 
   constructor(cards1:string[], cards2:string[]){
     this.cardsP1 = cards1;
@@ -11,7 +13,18 @@ class Game{
   }
 
   play():string {
-    return "";
+    for (let i =0; i < 2 ; i++){
+      let result:string = cards.compare(this.cardsP1[i], this.cardsP2[i]);
+      if(result === this.cardsP1[i])
+        this._pointsP1 +=1;
+      if(result === this.cardsP2[i])
+        this._pointsP2 +=1;
+    }
+    if(this._pointsP1 > this._pointsP2)
+      return "Player 1"
+    if(this._pointsP1 < this._pointsP2)
+      return "Player 2"
+    return "Draw"
   }
 }
 
