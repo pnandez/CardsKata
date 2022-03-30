269300dd75fa0965b85c8aea98e9737f237cd5d4
Pablo <alu0101225364@ull.edu.es>
2022-03-30 13:04:34 +0100

    Test compare two numbered cards green

--- src/cards.ts
+++ src/cards.ts
@@ -1,8 +1,9 @@
 class Cards{
 
   compare(card1:string, card2:string): string{
-    return "";
-  }
+    if(Number(card1) > Number(card2)){
+      return card1;
+    } else return card2;  }
 }
 
 export default new Cards();
