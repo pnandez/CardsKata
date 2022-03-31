8918188a24a22fc76c655a02a0d24fa7e2d9920a
pnandez <paquiherca@gmail.com>
2022-03-31 09:48:02 +0100

    Test cards check input in red

--- tests/cards.spec.ts
+++ tests/cards.spec.ts
@@ -1,11 +1,17 @@
 import Card from "../src/cards";
 import cards from "../src/cards";
 
-describe("Cards should", () =>{
+describe("Card should", () =>{
   test("have its own value", () =>{
     let carta1 = new Card("1");
 
     expect(carta1.getValue()).toBe(1);
 
+  })
+
+  test("throw exception when value is not in range", () => {
+    expect(() => {
+      let carta1 = new Card("0");
+    }).toThrow("You must provide a valid card name between 1-10 or J,Q,K")
   })
 })
