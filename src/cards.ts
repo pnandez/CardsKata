4e060980d7dc5770dd74d4816b0638cfe2ff7f47
pnandez <paquiherca@gmail.com>
2022-03-31 09:38:52 +0100

    Test cards show its value in green

--- src/cards.ts
+++ src/cards.ts
@@ -1,14 +1,16 @@
 
+const cardValuesDictionary: {[key:string]:number} = {"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13}
 class Card{
   private readonly name:string = ""
   private readonly value:number = 0;
 
   constructor(cardName:string){
-
+    this.name =  cardName;
+    this.value = cardValuesDictionary[cardName];
   }
 
   getValue():number{
-    return 0;
+    return this.value;
   }
 }
 
