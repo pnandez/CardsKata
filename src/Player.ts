0656dd3ec7ea71d20329a9414b01330cfe9dac95
pnandez <paquiherca@gmail.com>
2022-03-31 10:04:43 +0100

    Test cards check input in red

---
+++ src/Player.ts
@@ -0,0 +1,14 @@
+import Card from "./cards";
+
+class Player{
+
+  constructor(cardName1:string, cardName2:string){
+
+  }
+
+  getCards():Card[]{
+    return [];
+  }
+}
+
+export default Player;
--- tests/cards.spec.ts
+++ tests/cards.spec.ts
@@ -1,5 +1,5 @@
 import Card from "../src/cards";
-import cards from "../src/cards";
+import Player from "../src/Player";
 
 describe("Card should", () =>{
   test("have its own value", () =>{
@@ -13,5 +13,12 @@ describe("Card should", () =>{
     expect(() => {
       let carta1 = new Card("0");
     }).toThrow("You must provide a valid card name between 1-10 or J,Q,K")
+  })
+})
+
+describe("Player should", () =>{
+  test("be given two cards", () => {
+    let player1:Player = new Player("1","3");
+    expect(player1.getCards().length).toBe(2);
   })
 })
